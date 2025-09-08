c61d4c0e867ab32975e60ad7b3f25434fae75355
Camila Careli <camilascareli@gmail.com>
2025-09-08 17:15:41 -0300

    fix: Alterar data

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const targetDate = new Date("2025-09-10T09:00:00").getTime();
+const targetDate = new Date("2025-09-25T09:00:00").getTime();
 
 function updateCountdown() {
     const now = new Date().getTime();
